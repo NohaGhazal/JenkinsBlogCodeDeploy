93887bd37d9ab6eca77cbcc76b2e06252932e041
NohaGhazal <37781506+NohaGhazal@users.noreply.github.com>
2021-04-21 09:32:11 +0100

    Update mkdir.sh

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
-python -m pip install Flask
+python3.7 get-pip.py --user
+python3.7 -m pip install Flask
 #sdssdsds
